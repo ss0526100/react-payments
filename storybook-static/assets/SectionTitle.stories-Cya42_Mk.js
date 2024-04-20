728bb23b8d9c89f8c4fc291588a3ad21b16a0f09
ss0526100 <ss0526100@gmail.com>
2024-04-20 21:43:44 +0900

    Updates

--- storybook-static/assets/SectionTitle.stories-Cya42_Mk.js
+++
@@ -1,5 +0,0 @@
-import{S as a}from"./SectionTitle-BCsiONSy.js";import"./emotion-react.browser.esm-C_wMcisN.js";import"./jsx-runtime-Bdgnjw47.js";import"./index-Ccz02SFX.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-D8_Koz8h.js";const p={title:"SectionTitle",component:a},t={args:{title:""}};var e,r,o;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    title: ""
-  }
-}`,...(o=(r=t.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const l=["Default"];export{t as Default,l as __namedExportsOrder,p as default};
--- storybook-static/assets/emotion-use-insertion-effect-with-fallbacks.browser.esm-D8_Koz8h.js
+++
@@ -1,1 +0,0 @@
-import{r as s,R as a}from"./index-Ccz02SFX.js";var r=function(t){return t()},e=a.useInsertionEffect?a.useInsertionEffect:!1,f=e||r,o=e||s.useLayoutEffect;export{o as a,f as u};
